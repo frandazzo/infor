b9593d32fbecb9cdecc20c79daf20025c3c5371b
Francesco Randazzo <fg.randazzo@hotmail.it>
2017-11-17 15:26:05 +0100

    ultimo esempio jquery e intro ad angular

--- assets/js/mymath.js
+++ assets/js/mymath.js
@@ -1,13 +1,16 @@
 (function(){
 
 	var ns = window.infor || {};
+	window.infor = ns;
 	ns.myMath = {};
 
 	ns.myMath.somma = function(a, b){
 		return parseFloat(a) + parseFloat(b);
 	}
 
-	window.infor = ns;
+
+	
 
 
 })();
+
---
+++ assets/js/uiComponents.js
@@ -0,0 +1,34 @@
+//componente sommatore
+(function(){
+
+	var ns = window.infor || {};
+	window.infor = ns;
+	ns.uiComponents = window.infor.uiComponents  || {};
+
+	ns.uiComponents.sommatore = {};
+
+	ns.uiComponents.sommatore.init = function(resultLabelSelector, addendo1InputSelector, addendo2InputSelector){
+		
+
+		$(addendo1InputSelector ).keydown(function(){
+
+			$(resultLabelSelector).html(
+					window.infor.myMath.somma($(addendo1InputSelector).val(), $(addendo2InputSelector).val())
+				);
+			
+		});
+
+		$(addendo2InputSelector ).keydown(function(){
+
+			$(resultLabelSelector).html(
+					window.infor.myMath.somma($(addendo1InputSelector).val(), $(addendo2InputSelector).val())
+				);
+			
+		});
+	}
+
+	
+	
+
+
+})();
