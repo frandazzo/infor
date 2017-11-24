d931bcc9a4f343fe2b436138761a101e4bb63c7a
Francesco Randazzo <fg.randazzo@hotmail.it>
2017-11-24 12:09:45 +0100

    esempi scopes, watcher e dependency injection

---
+++ assets/js/inforAngularMiddleware.js
@@ -0,0 +1,71 @@
+angular.module('inforMiddleware',[])
+	.service('mathService', function(){
+
+		this.gotResults = [];
+		this.executedAlmostOneTime = 'no';
+
+		var somma = function(a1, a2){
+			this.executedAlmostOneTime = 'si';
+			var result = parseFloat(a1) + parseFloat(a2);
+			this.gotResults.push('in data ' + formatDate(new Date()) + ' è stato aggiunto il risultato: ' + result);
+			return result;
+		}
+
+		function formatDate(date) {
+			  var hours = date.getHours();
+			  var minutes = date.getMinutes();
+			  var ampm = hours >= 12 ? 'pm' : 'am';
+			  hours = hours % 12;
+			  hours = hours ? hours : 12; // the hour '0' should be '12'
+			  minutes = minutes < 10 ? '0'+minutes : minutes;
+			  var strTime = hours + ':' + minutes + ' ' + ampm;
+			  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+			}
+
+
+
+
+
+
+		this.somma = somma;
+
+	}).factory('mathService1', function(){
+
+		var gotResults = [];
+		var executedAlmostOneTime = 'no';
+
+
+		function formatDate(date) {
+			  var hours = date.getHours();
+			  var minutes = date.getMinutes();
+			  var ampm = hours >= 12 ? 'pm' : 'am';
+			  hours = hours % 12;
+			  hours = hours ? hours : 12; // the hour '0' should be '12'
+			  minutes = minutes < 10 ? '0'+minutes : minutes;
+			  var strTime = hours + ':' + minutes + ' ' + ampm;
+			  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+			}
+
+
+
+
+
+
+
+
+
+		var somma = function(a1, a2){
+			executedAlmostOneTime = 'si';
+			var result = parseFloat(a1) + parseFloat(a2);
+			gotResults.push('in data ' + formatDate(new Date()) + ' è stato aggiunto il risultato: ' + result);
+			return result;
+		}
+
+		
+		return {
+			somma:somma,
+			gotResults: gotResults,
+			executedAlmostOneTime: executedAlmostOneTime
+		};
+
+	});
---
+++ assets/js/sommatoreController.js
@@ -0,0 +1,36 @@
+var app = angular.module('sommatore', ['inforMiddleware']);
+app.controller('sommatoreController', ['$scope', 'mathService',function($scope, mathService1){
+	$scope.calcola = function(){
+		$scope.result = mathService1.somma($scope.addendo1, $scope.addendo2); //parseFloat($scope.addendo1) + parseFloat($scope.addendo2);
+		
+		if ($scope.result > 25){
+			$scope.coloredResult = null;
+		}else{
+			$scope.coloredResult = true;
+		}
+	}
+}]);
+
+app.controller('historyController', ['$scope','mathService', function($scope, mathService1){
+
+
+	$scope.history = mathService1.gotResults;
+	//$scope.executedAlmostOneTime = mathService1.executedAlmostOneTime;
+	$scope.$watch(function(){
+		return mathService1.executedAlmostOneTime;
+	},
+		function(newValue, oldValue, scope){
+			console.log('new val: ' +newValue);
+			console.log('old val: ' +oldValue);
+
+			console.log('scope: ' +scope);
+
+			$scope.executedAlmostOneTime = newValue;
+		})
+
+	
+
+
+
+
+}]);
